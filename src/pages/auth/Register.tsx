8ed3f0f68db3360322bfa71f206f725119980252
kasantix-design <kasantix@gmail.com>
2025-11-15 13:10:21 +0100

    Create Register.tsx

---
+++ src/pages/auth/Register.tsx
@@ -0,0 +1,8 @@
+export default function Register() {
+  return (
+    <div className="content-box">
+      <h2>Registrer ny bruker</h2>
+      <p>Dette er kun tilgjengelig for administrator (eller ved invitasjon).</p>
+    </div>
+  )
+}
