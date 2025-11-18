cb2f3f1b1ac184cf20317ad07df619d7aa23f0fc
kasantix-design <kasantix@gmail.com>
2025-11-18 11:00:17 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000
+    port: 3000,
+    strictPort: true  // Sikrer at Vite feiler hvis port 3000 er opptatt
   },
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    sourcemap: true   // Nyttig for debugging i prod
   }
 })
