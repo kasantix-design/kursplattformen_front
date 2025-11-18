67c674652dfa2a30d15b5e14e2d572f3a14ae2fa
kasantix-design <kasantix@gmail.com>
2025-11-18 13:13:56 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 // src/App.tsx
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import { useState } from "react";
 import Notification from "./components/Notification";
 import AppRoutes from "./router";
 
 export default function App() {
   const [notis, setNotis] = useState("");
+  const location = useLocation();
+  const isLoginPage = location.pathname === "/login";
+  const isLoggedIn = !!localStorage.getItem("token");
 
   return (
     <>
-      <Navbar />
+      {isLoggedIn && !isLoginPage && <Navbar />}
       <AppRoutes />
-      <Footer />
+      {isLoggedIn && !isLoginPage && <Footer />}
       {notis && <Notification message={notis} onClose={() => setNotis("")} />}
     </>
   );
