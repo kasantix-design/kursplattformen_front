55afbb5a63f41695d3c43c798640f8d2ae8faa0e
kasantix-design <kasantix@gmail.com>
2025-11-18 10:59:16 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
-import{defineConfig} from'vite'
+// vite.config.ts
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig ({
-  pligins: [react()],
-  server: 3000
-},
-build: {
-  outDir: 'dist'
-}
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000
+  },
+  build: {
+    outDir: 'dist'
+  }
 })
